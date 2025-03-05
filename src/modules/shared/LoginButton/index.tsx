dbf5382381a089c0fc5465a92d48402e24f37da8
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-03-05 23:47:32 +0100

    added thanks page

--- src/modules/shared/LoginButton/index.tsx
+++ src/modules/shared/LoginButton/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './styles/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginButtonProps {
   style?: React.CSSProperties;
 }
 
 export const LoginButton = ({ style }: LoginButtonProps) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/thanks');
+  };
   return (
-    <button className="main-section-button" style={style}>
+    <button className="main-section-button" onClick={handleClick} style={style}>
       Записатися
     </button>
   );
