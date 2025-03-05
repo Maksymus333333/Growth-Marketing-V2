2df02c6a31a4ed6d1de9866a2ee92082abf68607
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-03-05 21:14:42 +0100

    added main section

--- src/modules/AboutСourse/MainSection/index.tsx
+++ src/modules/AboutСourse/MainSection/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 import { JSX } from 'react';
+import './styles/styles.css';
+import { LoginButton } from '../../shared/LoginButton';
 
 export const MainSection = (): JSX.Element => {
   return (
-    <section className="main_section-wrapper">
-      <div>
-        <h1> Шукаю початківців для менторства у сфері Growth Marketing Associate </h1>
+    <section className="main-section-wrapper">
+      <div className="main-section-group">
+        <div className="main-section-title">
+          <h1 className="main-section-h1">
+            Шукаю початківців для менторства у сфері
+            <br />
+            <p> Growth Marketing Associate </p>
+          </h1>
+          <p className="main-section-description">
+            Привіт! Я хочу допомогти тим, хто тільки починає свій шлях у сфері росту та маркетингу.
+          </p>
+          <LoginButton />
+        </div>
       </div>
     </section>
   );
---
+++ src/modules/shared/LoginButton/index.tsx
@@ -0,0 +1,6 @@
+import React, { JSX } from 'react';
+import './styles/styles.css';
+
+export const LoginButton = (): JSX.Element => {
+  return <button className="main-section-button"> Записатися </button>;
+};
