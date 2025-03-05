7e0a91bdb9fd7d81cd322b4b3776ec6ecd85f6fc
amslavko <amslavko@gmail.com>
2025-03-06 00:06:31 +0100

    fixed conflicts + section Proposition

--- src/modules/AboutСourse/Propositions/index.tsx
+++ src/modules/AboutСourse/Propositions/index.tsx
@@ -9,7 +9,13 @@ import OfficeWorker from '../../../assets/icons/office-worker.svg';
 import GroupMeeting from '../../../assets/icons/group-meeting.svg';
 import Handshake from '../../../assets/icons/business-handshake.svg';
 
-const PropositionItem = ({ icon, text, containerClass }: { icon: string; text: string; containerClass: string }) => (
+interface PropositionItemProps {
+  icon: string;
+  text: string;
+  containerClass: string;
+}
+
+const PropositionItem: React.FC<PropositionItemProps> = ({ icon, text, containerClass }) => (
   <div className={containerClass}>
     <div className='icon-list-container'>
       <img src={icon} alt='icon' className='proposition-icon' />
@@ -17,6 +23,14 @@ const PropositionItem = ({ icon, text, containerClass }: { icon: string; text: s
     <p className='icon-list-text'>{text}</p>
   </div>
 );
+
+const propositionsData = [
+  { icon: TaskList, text: 'Практичні завдання, які підготують до реальної роботи.', containerClass: 'proposition-icon-text-pink-container' },
+  { icon: Collaborations, text: 'Персоналізовану навчальну програму, адаптовану під твій рівень.', containerClass: 'proposition-icon-text-blue-container' },
+  { icon: OfficeWorker, text: 'Допомогу в створенні портфоліо та розвитку професійних навичок.', containerClass: 'proposition-icon-text-blue-container' },
+  { icon: GroupMeeting, text: 'Регулярні менторські сесії для розбору складних тем і підтримки.', containerClass: 'proposition-icon-text-pink-container' },
+  { icon: Handshake, text: 'Сприяння в працевлаштуванні на прямі контракти в закордонні компанії з місячною зарплатою від $4,000 до $6,500.', containerClass: 'proposition-icon-text-pink-container' },
+];
 
 export const Propositions = (): JSX.Element => {
   return (
@@ -26,37 +40,19 @@ export const Propositions = (): JSX.Element => {
           <h2 className='proposition-h2'>Що я пропоную?</h2>
           <img src={PinkPath} className='pink-path' alt='PinkPath icon' />
         </div>
-        <PropositionItem 
-          icon={TaskList} 
-          text='Практичні завдання, які підготують до реальної роботи.'
-          containerClass='proposition-icon-text-pink-container'
-        />
+        <PropositionItem {...propositionsData[0]} />
       </div>
 
       <div className='middle-proposition-container'>
-        <PropositionItem 
-          icon={Collaborations} 
-          text='Персоналізовану навчальну програму, адаптовану під твій рівень.'
-          containerClass='proposition-icon-text-blue-container'
-        />
-        <PropositionItem 
-          icon={OfficeWorker} 
-          text='Допомогу в створенні портфоліо та розвитку професійних навичок.'
-          containerClass='proposition-icon-text-blue-container'
-        />
+        {propositionsData.slice(1, 3).map((item, index) => (
+          <PropositionItem key={index} {...item} />
+        ))}
       </div>
 
       <div className='right-proposition-container'>
-        <PropositionItem 
-          icon={GroupMeeting} 
-          text='Регулярні менторські сесії для розбору складних тем і підтримки.'
-          containerClass='proposition-icon-text-pink-container'
-        />
-        <PropositionItem 
-          icon={Handshake} 
-          text='Сприяння в працевлаштуванні на прямі контракти в закордонні компанії з місячною зарплатою від $4,000 до $6,500.'
-          containerClass='proposition-icon-text-pink-container'
-        />
+        {propositionsData.slice(3).map((item, index) => (
+          <PropositionItem key={index} {...item} />
+        ))}
       </div>
     </section>
   );
